657420688114cafde6beeeb92bc1e8331ffae5ac
Al McKinlay <alistair@10people.co.uk>
2018-01-23 10:01:16 +0000

    Add checkbox to items

--- src/templates/item.js
+++ src/templates/item.js
@@ -1,10 +1,30 @@
 import React from "react";
 import { ListGroupItem } from 'reactstrap';
+import { Icon } from "react-font-awesome-5";
 
-export const Item = ({ name, help }) => {
-	return (
-		<ListGroupItem>
-			{name}
-		</ListGroupItem>
-	);
-};
+export class Item extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+			selected: false
+		}
+	}
+
+	toggle() {
+		this.setState({
+			selected: !this.state.selected
+		})
+	}
+
+	render() {
+		return (
+			<ListGroupItem onClick={this.toggle}>
+				{this.state.selected ? <Icon.CheckSquare.regular /> : <Icon.Square.regular />}
+				{this.props.name}
+			</ListGroupItem>
+		);
+	}
+}
