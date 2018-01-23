0e613db008be0348d4753759ba372fd5c7587d0c
Al McKinlay <alistair@10people.co.uk>
2018-01-23 09:15:49 +0000

    Move header to layouts dir

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
-import Header from '../components/Header';
+import Header from './header';
 import './index.css';
 
 const TemplateWrapper = ({ children }) => (
