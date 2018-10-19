f72bbd5d399b697f4ef8b47b1788ab140656c9fe
Al McKinlay <github@10people.co.uk>
2018-10-19 12:16:30 +0100

    Add meta tags for og and twitter (#76)

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,15 +6,37 @@ import Header from './header';
 import './index.css';
 import 'react-circular-progressbar/dist/styles.css';
 
+import screenshot from './screenshot.png';
+
+const data = {
+  url: "https://toolbox.yamanickill.com",
+  title: "The Completionist's Toolbox",
+  description: "A tool for completionists of all types",
+  image: screenshot
+}
+
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet
-      title="The Completionist Toolbox"
-      meta={[
-        { name: 'description', content: 'A tool for completionists of all types' },
-        { name: 'keywords', content: 'gaming, completionists' },
-      ]}
-    />
+    <Helmet>
+      <title>{data.title}</title>
+      <meta name="description" content={data.description} />
+      <meta name="image" content={data.image} />
+      <meta name="keywords" content="gaming, completionists" />
+
+      {/* OpenGraph tags */}
+      <meta property="og:url" content={data.url} />
+      <meta property="og:type" content="website" /> : null}
+      <meta property="og:title" content={data.title} />
+      <meta property="og:description" content={data.description} />
+      <meta property="og:image" content={data.image} />
+
+      {/* Twitter Card tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@TheScotBot" />
+      <meta name="twitter:title" content={data.title} />
+      <meta name="twitter:description" content={data.description} />
+      <meta name="twitter:image" content={data.image} />
+    </Helmet>
     <Header />
     <main>
       <div className={"container mt-4"}>
