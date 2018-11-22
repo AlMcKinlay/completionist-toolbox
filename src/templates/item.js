62acf28c2b5b166db8051734292fda2f5d140239
Alistair McKinlay <github@10people.co.uk>
2018-11-22 15:43:43 +0000

    Switch to controlled tooltip for help text, in attempt to fix the issue in prod

--- src/templates/item.js
+++ src/templates/item.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { UncontrolledTooltip } from 'reactstrap';
+import { Tooltip } from 'reactstrap';
 import { ThemedListItem } from '../components/ThemedList';
 import { Icon } from "react-font-awesome-5";
 
@@ -13,9 +13,16 @@ export class Item extends React.Component {
 		super(props);
 
 		this.copy = this.copy.bind(this);
+		this.toggle = this.toggle.bind(this);
 		this.state = {
-			copied: false
+			copied: false,
+			tooltipOpen: false
 		}
+	}
+	toggle() {
+	  this.setState({
+		tooltipOpen: !this.state.tooltipOpen
+	  });
 	}
 
 	copy(e) {
@@ -48,12 +55,12 @@ export class Item extends React.Component {
 				{this.props.help &&
 					<span className="float-right" onClick={this.copy}>
 						<Icon.QuestionCircle.regular id={`item-tooltip-${this.getName()}`} />
-						<UncontrolledTooltip placement="right" target={`item-tooltip-${this.getName()}`} >
+						<Tooltip isOpen={this.state.tooltipOpen} placement="right" target={`item-tooltip-${this.getName()}`} toggle={this.toggle} >
 							<span>
 								<span ref={(helpText) => { this.helpText = helpText; }}>{this.props.help}</span>
 								{this.props.help.startsWith("http") ? this.state.copied ? " (copied)" : " (click to copy)" : ""}
-								</span>
-						</UncontrolledTooltip>
+							</span>
+						</Tooltip >
 					</span>
 				}
 			</ThemedListItem>
