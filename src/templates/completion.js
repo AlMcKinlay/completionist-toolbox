bd5b9b68eb079ba8ff955e79b84f91fe8b5e66cf
Alistair McKinlay <github@10people.co.uk>
2019-06-17 14:52:51 +0100

    Fix breaking changes

--- src/templates/completion.js
+++ src/templates/completion.js
@@ -1,5 +1,5 @@
 import React from "react";
-import CircularProgressbar from 'react-circular-progressbar';
+import { CircularProgressbar } from 'react-circular-progressbar';
 
 export class Completion extends React.Component {
 	constructor(props) {
@@ -22,7 +22,7 @@ export class Completion extends React.Component {
 	render() {
 		return (
             <CircularProgressbar
-                percentage={this.getPercentage()}
+                value={this.getPercentage()}
                 text={`${this.getPercentage()}%`}
             />
 		);
