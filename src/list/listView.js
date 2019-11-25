bc6076d52886bac79b61011c5291393a06c177a2
Al McKinlay <github@10people.co.uk>
2019-11-25 15:06:44 +0000

    Protocol can sometimes be "https:"

--- src/list/listView.js
+++ src/list/listView.js
@@ -136,7 +136,7 @@ class SectionList extends React.Component {
 		const id = urlId || (props.listState && props.listState.id);
 
 		if (typeof WebSocket !== "undefined") {
-			const protocol = window.location.protocol === "https" ? "wss" : "ws";
+			const protocol = window.location.protocol.startsWith("https") ? "wss" : "ws";
 			const socket = new WebSocket(`${protocol}://${window.location.hostname}:3000/`);
 			socket.onmessage = (event) => {
 				const message = JSON.parse(event.data);
