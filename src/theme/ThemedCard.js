1a09b553aa2eef5cedf8be1efbcc129f88701a45
Al McKinlay <github@10people.co.uk>
2022-03-12 19:03:50 +0000

    Fix breaking changes

--- src/theme/ThemedCard.js
+++ src/theme/ThemedCard.js
@@ -5,17 +5,17 @@ export const ThemedCard = styled(Card)`
 	height: 100%;
 
 	background-color: ${props => props.theme.background()};
-	${props => props.theme.dark && 'border-color: rgba(255, 255, 255, 0.25);'}
-	
-	${props => props.type === "blank" && 'border-style: dashed;'}
-	${props => props.type === "blank" && 'cursor: pointer;'}
+	${props => props.theme.dark && 'border-color: rgba(255, 255, 255, 0.25)'};
+
+	${props => props.type === "blank" && 'border-style: dashed'};
+	${props => props.type === "blank" && 'cursor: pointer'};
 	&:hover {
-		${props => props.type === "blank" && `background-color: ${props.theme.hoverBackground()};`}
+		${props => props.type === "blank" && `background-color: ${props.theme.hoverBackground()}`};
 	}
 `;
 
 export const ThemedCardHeader = styled(CardHeader)`
 	background-color: ${props => props.theme.background()};
   
-	${props => props.theme.dark && 'border-color: rgba(255, 255, 255, 0.25);'}
+	${props => props.theme.dark && 'border-color: rgba(255, 255, 255, 0.25)'};
 `;
